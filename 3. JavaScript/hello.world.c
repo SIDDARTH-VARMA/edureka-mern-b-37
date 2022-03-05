a608eb7b49964ce36a88be2387356cead1183149
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-03-05 20:28:17 +0530

    Starting with JS

---
+++ 3. JavaScript/hello.world.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main(){
+    // Statically Typed Lang
+    int num = 10;
+    char[] name="Dheeraj";
+
+    printf("Hello world");
+}
+
---
+++ 3. JavaScript/variables.js
@@ -0,0 +1,15 @@
+
+// How to declare a variable in JS ?
+// var means Variable.
+
+// How to declare a constant in JS ?
+
+let num = 10;
+num = 20;
+
+let myname="My name";
+console.log(myname);
+console.log(num);
+
+const marks=100;
+console.log(marks);
