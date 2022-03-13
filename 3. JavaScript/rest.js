71424b8c46ad23cb58dc3ac0ba27587eb52060c3
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-03-13 19:53:02 +0530

    rest & spread

--- 3. JavaScript/rest.js
+++ 3. JavaScript/rest.js
@@ -3,11 +3,16 @@
 
 // Used in collections to help with multiple values.
 
-// Using spread operator
+// Using rest operator
 // Converts individual values into an array.
 function sum(...numbers){
     let result=0;
     for(let i=0; i<numbers.length; i++){
+        // result = 0+2 = 2; // 1st
+        // result = 2+3 = 5; // 2nd
+        // result = 5+4 = 9; // 3rd
+        // result = 9+5 = 14; // 4th
+        // result = 14+6 = 20; // 5th
         result=result+numbers[i];
     }
     console.log(result);
@@ -16,3 +21,4 @@ function sum(...numbers){
 sum(2, 3); // 5
 sum(2, 3, 4); // 9
 sum(2, 3, 4, 5); // 14
+sum(2, 3, 4, 5, 6); // 20
