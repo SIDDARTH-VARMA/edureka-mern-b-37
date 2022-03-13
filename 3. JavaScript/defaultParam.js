c21153dc609c01f1ccc2dc96a6c649ed3118f767
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-03-13 20:22:38 +0530

    default

--- 3. JavaScript/defaultParam.js
+++ 3. JavaScript/defaultParam.js
@@ -11,4 +11,5 @@ function calculateTotalPrice(costOfFood, Taxes, deliveryCharges=10){
 calculateTotalPrice(400, 20, 30);
 
 // pro membership.
-calculateTotalPrice(400, 20, 50);
+calculateTotalPrice(400, 20); // 430. Uses 10 for delivery charges.
+calculateTotalPrice(400, 20, 50); // 470, uses 50 as delivery charges. 
