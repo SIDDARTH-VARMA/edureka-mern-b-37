177820405d64febf0c42b7d3e1b9ca928fbd5985
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-03-26 19:50:23 +0530

    radio element

--- 3. JavaScript/Forms.js
+++ 3. JavaScript/Forms.js
@@ -8,3 +8,27 @@ function handleNameChange(){
     // update label
     document.getElementById("lblNameField").innerText = name;
 }
+
+// Handle when name field changes
+function handleUserNameChange(){
+    // 1. get name element and its value
+    const name = document.getElementById("inputUserName").value;
+    console.log(name);
+    // update label
+    document.getElementById("lblUsernameField").innerText = name;
+}
+
+function handleCountryChange(){
+    const country = document.getElementsByClassName("form-select")[0];
+    console.log(country);
+    const labelValue = country.options[country.selectedIndex].text;
+    document.getElementById("ddlCountryLabel").innerText=labelValue;
+}
+
+function handleGenderChange(){
+    // get both elements by using getElementsByName
+    const genderElements = document.getElementsByName("gender");
+    const isMale = genderElements[0].checked;
+    const isFeMale = genderElements[1].checked;
+    document.getElementById("lblGenderField").innerText=isMale ? "Male" : "Female";
+}
