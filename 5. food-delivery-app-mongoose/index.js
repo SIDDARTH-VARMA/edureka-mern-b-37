66c629beb4eaf3988e9245021ebd50f8cb063e5a
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-05-15 18:54:45 +0530

    mongoose add/get

--- 5. food-delivery-app-mongoose/index.js
+++ 5. food-delivery-app-mongoose/index.js
@@ -2,11 +2,13 @@
 const express = require("express");
 const router = require("./src/routes/restaurant");
 const mongooseConfig = require("./config/mongodb");
+const bodyParser= require("body-parser");
 
 const server = express();
 mongooseConfig.mongooseConnect();
 server.listen(4500);
 
+server.use(bodyParser.json());
 server.use("/api/restaurant", router);
 
 server.get("/", (req, res)=>{
--- 5. food-delivery-app-mongoose/src/routes/restaurant.js
+++ 5. food-delivery-app-mongoose/src/routes/restaurant.js
@@ -1,9 +1,11 @@
 
 const express = require("express");
 const router = express.Router();
+const controller = require("../controllers/restaurant");
 
-router.post("/", );
-router.post("/menu", );
+router.post("/", controller.add);
+router.get("/", controller.get);
+// router.post("/menu", );
 
 
 module.exports = router;
